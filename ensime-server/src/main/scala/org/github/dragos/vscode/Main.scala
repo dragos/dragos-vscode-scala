bd96aba4efc028921100ee3c1e6909f03f2b2cbe
Iulian Dragos <jaguarul@gmail.com>
2016-11-05 19:24:35 +0100

    Route stdout to a file.
    The presentation compiler sometimes prints stuff on stdout.
    
    Hopefully fix #6.

--- ensime-server/src/main/scala/org/github/dragos/vscode/Main.scala
+++ ensime-server/src/main/scala/org/github/dragos/vscode/Main.scala
@@ -2,14 +2,27 @@ package org.github.dragos.vscode
 
 import com.typesafe.scalalogging.LazyLogging
 import scala.util.Properties
+import java.io.PrintStream
+import java.io.FileOutputStream
 
 object Main extends LazyLogging {
   def main(args: Array[String]): Unit = {
-    logger.info(s"Starting server in ${System.getenv("PWD")}")
+    val cwd = System.getenv("PWD")
+    logger.info(s"Starting server in $cwd")
     logger.info(s"Classpath: ${Properties.javaClassPath}")
 
     val server = new EnsimeLanguageServer(System.in, System.out)
-    server.start()
+
+    // route System.out somewhere else. The presentation compiler may spit out text
+    // and that confuses VScode, since stdout is used for the language server protocol
+    val origOut = System.out
+    try {
+      System.setOut(new PrintStream(new FileOutputStream(s"$cwd/pc.stdout.log")))
+      println("This file contains stdout from the presentation compiler.")
+      server.start()
+    } finally {
+      System.setOut(origOut)
+    }
 
     // make sure we actually exit
     System.exit(0)
